6e5ccd4197b563a0bc0d9593487297adc70324f7
varunken42 <111035240+varunken42@users.noreply.github.com>
2022-12-09 16:39:05 +0530

    changes on ltpct

--- src/main/java/com/ken42/documentupload.java
+++ src/main/java/com/ken42/documentupload.java
@@ -99,7 +99,7 @@ public class documentupload {
             throws Exception {
         try {
             System.out.println("TC-3: Fill form with validation test started Executation ");
-            login(url, driver, csvCell);
+            // login(url, driver, csvCell);
 
             fess.sf_login(sfurl, driver, csvCell, log);
 
